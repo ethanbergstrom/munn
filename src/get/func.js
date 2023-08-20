a112f1f27b371539cab09d29d712c63c4b3199bc
Ethan Bergstrom <13603162+ethanbergstrom@users.noreply.github.com>
2023-08-20 18:00:58 +0000

    Make it even more flexible

--- src/get/func.js
+++ src/get/func.js
@@ -10,15 +10,19 @@ const client = new NoSQLClient({
 });
 
 const allowedAttributes = ['temperature', 'lux', 'pressure'];
+const allowedMinutes = 180;
 
-// Retrieve the last 3 hours
-const minuteSpan = 180;
+// Retrieve the last hour by default
+const defaultMinutes = 60;
 // JS does time arithmetic in milliseconds
 const msPerMinute = 60000;
-const timeSpan = minuteSpan * msPerMinute;
 
 fdk.handle(async function (input) {
 	try {
+		// If a specific timespan (in minutes) was requested and within the allowed limit, use it. Otherwise, use the default. 
+		const reqMinutes = input['minutes']
+		const timeSpan = (reqMinutes && reqMinutes < allowedMinutes) ? reqMinutes : defaultMinutes
+
 		var resultSet = []
 		const enviroAttributes = input.attributes.filter(value => allowedAttributes.includes(value));
 		const query = `SELECT collectedAt,${enviroAttributes} FROM ${process.env.TABLE_NAME} WHERE collectedAt > '${new Date(new Date() - timeSpan).toISOString()}' ORDER BY collectedAt`
